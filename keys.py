4ded3a64c8ae552cb2ff036d9a8f009acd21cab2
Mike Redrobe <mike@redrobe.net>
2018-05-10 15:14:03 +0100

    Update keys.py

--- keys.py
+++ keys.py
@@ -17,11 +17,13 @@ scancodes = {
     40: u'"', 41: u'`', 42: u'LSHFT', 43: u'\\', 44: u'z', 45: u'x', 46: u'c', 47: u'v', 48: u'b', 49: u'n',
     50: u'm', 51: u',', 52: u'.', 53: u'/', 54: u'RSHFT', 56: u'LALT', 57: u' ', 100: u'RALT'
 }
+# hidcodes from http://www.usb.org/developers/hidpage/Hut1_12v2.pdf p53
 
 #grab provides exclusive access to the device
 dev.grab()
 
-caps= False;
+deadkey=False
+caps= False
 hidkey =0
 
 #loop
@@ -33,20 +35,34 @@ for event in dev.read_loop():
                 caps = True
             if data.keystate == 0:
                 caps = False
+            continue # don't send
         if data.keystate == 1:  # Down events only
             key_lookup = u'{}'.format(scancodes.get(data.scancode))
 
-            print key_lookup, data.scancode
+            # print key_lookup, data.scancode
 
             if len(key_lookup) == 1 :
                hidkey = ord(key_lookup) - 93 # a-z
             if hidkey < 0: # 1-9
-                   hidkey += 74
+                   hidkey = 0
+
+                    
+            if data.scancode == 2: hidkey = 30 # 1
+            if data.scancode == 3: hidkey = 31 # 2
+            if data.scancode == 4: hidkey = 32 # 3
+            if data.scancode == 5: hidkey = 33 # 4
+            if data.scancode == 6: hidkey = 34 # 5
+            if data.scancode == 7: hidkey = 35 # 6
+            if data.scancode == 8: hidkey = 36 # 7
+            if data.scancode == 9: hidkey = 37 # 8
+            if data.scancode == 10: hidkey = 38 # 9
+            if data.scancode == 11: hidkey = 39 # 0
+
 
             if data.scancode == 57: hidkey = 44 # space
             if data.scancode == 14: hidkey = 42 # bkspc
             if data.scancode == 28: hidkey = 40 # enter
-            if data.scancode == 2: hidkey = 41 # ESC
+            if data.scancode == 1: hidkey = 41 # ESC
 
             if data.scancode == 106: hidkey = 79 # RIGHT
             if data.scancode == 105: hidkey = 80 # LEFT
@@ -66,10 +82,24 @@ for event in dev.read_loop():
             if data.scancode == 69: hidkey = 68 # F11
             if data.scancode == 70: hidkey = 69 # F12
 
+            if data.scancode == 12: hidkey = 45 # -
+            if data.scancode == 13: hidkey = 46 # =
+            if data.scancode == 15: hidkey = 43 # TAB
 
-            if data.scancode == 26: hidkey = 45 # -
+            if data.scancode == 26: hidkey = 47 # {
+            if data.scancode == 27: hidkey = 48 # ]
 
-            print key_lookup, data.scancode, hidkey
+            if data.scancode == 39: hidkey = 51 # :
+            if data.scancode == 40: hidkey = 52 # "
+
+            if data.scancode == 51: hidkey = 54 # <
+            if data.scancode == 52: hidkey = 55 # >
+            if data.scancode == 53: hidkey = 56 # ?
+
+            if data.scancode == 41: hidkey = 50 # #
+            if data.scancode == 43: hidkey = 49 # \
+
+            # print key_lookup, data.scancode, hidkey
 
             if caps:
                 write_report(chr(32)+NULL_CHAR + chr (hidkey) + NULL_CHAR*5)
